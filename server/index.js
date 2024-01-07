b5be05130f91af07a6a45848dd630078bb78a989
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-01-07 22:14:50 +0300

    local auth

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
-const { Client } = require("whatsapp-web.js");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 const app = express();
 const port = 3001;
 
-app.listen(port, () => {
-  console.log("server listening on port::${port}");
+const client = new Client({
+  puppeteer: {
+    headless: false,
+  },
+  authStrategy: new LocalAuth({
+    clientId: "CLIENT_ID",
+  }),
 });
 
-const client = new Client();
+client.on("qr", (qr) => {
+  console.log("QR received", qr);
+});
+
+client.on("ready", () => {
+  console.log("client is ready");
+});
+
+client.initialize();
+
+app.listen(port, () => {
+  console.log(`server listening on port::${port}`);
+});
