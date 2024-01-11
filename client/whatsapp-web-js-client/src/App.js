5f0f5a56cf5ca8d475096d56a1bc2e9e2b34f8a8
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-01-11 16:46:30 +0300

    imp on wweb.js

--- client/whatsapp-web-js-client/src/App.js
+++ client/whatsapp-web-js-client/src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import QRCode from "react-qr-code";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [id, setId] = useState("");
+  const [session, setSession] = useState("");
+  const [qrCode, setQrCode] = useState("");
+  const [oldSessionId, setOldSessionId] = useState("");
+
+  const createSessionsForWp = () => {
+    socket.emit("createSession", {
+      id: session,
+    });
+  };
+
+  useEffect(() => {
+    socket.emit("connected", "hello from client");
+    socket.on("qr", (data) => {
+      const { qr } = data;
+      setQrCode(qr);
+    });
+
+    socket.on("ready", (data) => {
+      console.log("ready", data);
+      const { id } = data;
+      setId(id);
+    });
+
+    socket.on("allChats", (data) => {
+      console.log("allChats", data);
+    });
+  }, []);
+
+  const getOldSession = () => {
+    socket.emit("getSession", { id: oldSessionId });
+  };
+
+  const getAllChats = () => {
+    socket.emit("getAllChats", { id });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Whatsapp Web JS Client</h1>
+      <h1>Open Whatsapp and scan Qr Code</h1>
+      <div>
+        <input
+          type="text"
+          value={oldSessionId}
+          onChange={(e) => {
+            setOldSessionId(e.target.value);
+          }}
+        />
+        <button onClick={getOldSession}>Get old session</button>
+      </div>
+      <div style={{ marginBottom: "40px" }}>
+        <input
+          type="text"
+          value={session}
+          onChange={(e) => {
+            setSession(e.target.value);
+          }}
+        />
+        <button onClick={createSessionsForWp}>Create Session</button>
+      </div>
+
+      <div style={{ marginBottom: "24px" }}>
+        {id !== "" && <button onClick={getAllChats}>Get All Chats</button>}
+      </div>
+      {qrCode ? <QRCode value={qrCode} /> : null}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,27 +1,180 @@
 const express = require("express");
-const { Client, LocalAuth } = require("whatsapp-web.js");
+const { Client, LocalAuth, RemoteAuth } = require("whatsapp-web.js");
+const { Server } = require("socket.io");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
 const port = 3001;
+const OpenAI = require("openai");
+const { MongoStore } = require("wwebjs-mongo");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const client = new Client({
-  puppeteer: {
-    headless: false,
+let store;
+mongoose.connect(process.env.MONGO_KEY).then(() => {
+  console.log("Connect mongoDB");
+  store = new MongoStore({ mongoose: mongoose });
+  console.log("store", store);
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
   },
-  authStrategy: new LocalAuth({
-    clientId: "CLIENT_ID",
-  }),
 });
 
-client.on("qr", (qr) => {
-  console.log("QR received", qr);
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from client</h1>");
 });
 
-client.on("ready", () => {
-  console.log("client is ready");
+server.listen(port, () => {
+  console.log("listening on port *:3001");
 });
 
-client.initialize();
+const allSessionsObject = {};
+const createWhatsappSession = (id, socket) => {
+  const client = new Client({
+    puppeteer: {
+      headless: false,
+    },
+    authStrategy: new RemoteAuth({
+      clientId: id,
+      store: store,
+      backupSyncIntervalMs: 300000,
+    }),
+  });
 
-app.listen(port, () => {
-  console.log(`server listening on port::${port}`);
+  client.on("qr", (qr) => {
+    console.log("QR received", qr);
+    socket.emit("qr", { qr });
+  });
+
+  client.on("authenticated", () => {
+    console.log("Authenticated");
+  });
+
+  client.on("ready", () => {
+    console.log("client is ready");
+    allSessionsObject[id] = client;
+    socket.emit("ready", { id, message: "Client is ready!" });
+  });
+
+  client.on("remote_session_saved", () => {
+    console.log("Remote session saved");
+    socket.emit("remote_session_saved", {
+      message: "Remote session saved",
+    });
+  });
+
+  const openai = new OpenAI({
+    apiKey: process.env.API_KEY,
+  });
+
+  const chatHistory = {};
+
+  function updateChatHistory(userId, message, isUser) {
+    if (!chatHistory[userId]) {
+      chatHistory[userId] = [];
+    }
+    chatHistory[userId].push({
+      role: isUser ? "user" : "assistant",
+      content: message,
+    });
+  }
+
+  async function runCompletion(userId, message) {
+    try {
+      const messages = chatHistory[userId] ? [...chatHistory[userId]] : [];
+      messages.push({
+        role: "assistant",
+        content:
+          "you're my personal assistant (My name is faize and with this information you can return to people who contact me as 'I am faize's assistant'.). it's your duty to reply to my messages in a polite and courteous manner. ",
+      });
+      const response = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: messages,
+        max_tokens: 1000,
+      });
+      updateChatHistory(userId, response.choices[0].message.content, false);
+
+      return response.choices[0].message.content;
+    } catch (error) {
+      if (error instanceof OpenAI.APIError) {
+        console.error("Error with OpenAI API:", error.message);
+      } else {
+        console.error("Unexpected error:", error);
+      }
+    }
+  }
+
+  client.on("message", (message) => {
+    const userId = message.from;
+    updateChatHistory(userId, message.body, true);
+
+    runCompletion(userId, message.body).then((result) => {
+      message.reply(result);
+    });
+  });
+
+  client.initialize();
+};
+
+const getWhatsappSession = (id, socket) => {
+  const client = new Client({
+    puppeteer: {
+      headless: false,
+    },
+    authStrategy: new RemoteAuth({
+      clientId: id,
+      store: store,
+      backupSyncIntervalMs: 300000,
+    }),
+  });
+
+  client.on("ready", () => {
+    console.log("client is ready");
+    socket.emit("ready", { id, message: "client is ready" });
+  });
+
+  client.on("qr", (qr) => {
+    socket.emit("qr", {
+      id,
+      message: "user got logged out and here is another QR code",
+    });
+  });
+
+  client.initialize();
+};
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket?.id);
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("connected", (data) => {
+    console.log("connected to the server");
+    socket.emit("hello", "hello from server");
+  });
+
+  socket.on("createSession", (data) => {
+    console.log("created session", data);
+    const { id } = data;
+    createWhatsappSession(id, socket);
+  });
+
+  socket.on("getSession", (data) => {
+    console.log("getSession", data);
+    const { id } = data;
+    getWhatsappSession(id, socket);
+  });
+
+  socket.on("getAllChats", async (data) => {
+    console.log("getAllChats", data);
+    const { id } = data;
+    const client = allSessionsObject[id];
+    const allChats = await client.getChats();
+    socket.emit("AllChats", { allChats });
+  });
 });
